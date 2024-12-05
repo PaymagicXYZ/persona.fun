624eb7ceb5d71b8c18db66bcac726b9691b2b5d2
ealeksandrov7 <evlogi@weichain.tech>
2024-12-05 21:41:32 +0200

    chore: add utils tsup config

---
+++ packages/utils/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  minify: false,
+  splitting: false,
+  treeshake: true,
+  platform: 'node',
+  target: 'node23',
+}); 
