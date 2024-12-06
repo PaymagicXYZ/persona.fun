105cba0caa56f21bea02839530ec582def4f64c8
ealeksandrov7 <evlogi@weichain.tech>
2024-12-06 17:18:10 +0200

    fix: cron setup#9

--- packages/cron/tsup.config.ts
+++ packages/cron/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/update-merkle-tree.ts'],
   format: ['esm'],
   dts: true,
   clean: true,
