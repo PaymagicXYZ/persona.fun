ec25262066ac7ac80b24aa32cb4c568ba15c2a5d
ealeksandrov7 <evlogi@weichain.tech>
2024-12-07 23:07:30 +0200

    fix: supabase getRoom method#4

--- apps/eliza/src/index.ts
+++ apps/eliza/src/index.ts
@@ -1,3 +1,9 @@
+import { elizaLogger } from '@ai16z/eliza'
 export { SupabaseAdapterV2 } from './SupabaseAdapterV2'
-export * from './eliza'
 
+import { startAgents } from './eliza'
+
+startAgents().catch((error) => {
+  elizaLogger.error('Unhandled error in startAgents:', error)
+  process.exit(1)
+})
