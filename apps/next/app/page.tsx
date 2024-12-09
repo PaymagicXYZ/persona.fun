eb623a171be034a557adc1d19532688593f4328e
Andon Mitev <andon@weichain.tech>
2024-12-10 00:31:18 +0200

    fix: build

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import Banner from "@/components/banner";
-import TradingView from "@/components/dextools-chart-view";
+
 import Personas from "@/components/personas";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8">
-      {/* <TradingView /> */}
-
       <Banner />
       <Personas />
 
